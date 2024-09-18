a2a60cd6ac0143e94889e961f88450d6844fc8ce
batmanblack <isabellacruz376@gmail.com>
2024-09-18 12:10:44 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(600, 600);
+   background("white");
+  }
+  
+  function draw() {
+   
+    stroke("rgb(248,7,7)");
+    fill("black");
+   
+  //console.log(mouseIsPressed);
+   
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35,);
+      
+      
+    }
+  
+  }
